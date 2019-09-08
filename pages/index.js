714486eec03ccf609d32425853e03edc668e2aa8
ssunny <se7en-jo@hanmail.net>
2019-09-08 11:16:20 +0900

    useState 를 이용하여 로또번호 생성하기
    - 재생성 추가하기

--- pages/index.js
+++ pages/index.js
@@ -13,69 +13,85 @@ import _ from 'underscore'; // 관례적으로 .. 로드할 때는 같은 이름
 // underscore => _
 // jQuery => $
 
-let numbers = [];
-_.times(45, n => numbers.push(n + 1)); // 45로 했을 때 0 ~ 44 도는데 우리가 필요한 수는 1~45이기 때문에 push 할 때 1을 더해준 것
-numbers = _.shuffle(numbers); // _ : 원본을 변경하지 않고, 변경한 값을 리턴하는 형식입니다. 따라서 원본을 바꾸려면 다시 원본에 덮어주는 작업이 필요합니다.
-numbers.length = 6; // length : 읽기 전용의 값이 아니라 , 쓰기도 가능함 . 이렇게 하면 앞에서부터 6개를 자른다.
+function generate() {
+  let numbers = [];
+  _.times(45, n => numbers.push(n + 1)); // 45로 했을 때 0 ~ 44 도는데 우리가 필요한 수는 1~45이기 때문에 push 할 때 1을 더해준 것
+  numbers = _.shuffle(numbers); // _ : 원본을 변경하지 않고, 변경한 값을 리턴하는 형식입니다. 따라서 원본을 바꾸려면 다시 원본에 덮어주는 작업이 필요합니다.
+  numbers.length = 6; // length : 읽기 전용의 값이 아니라 , 쓰기도 가능함 . 이렇게 하면 앞에서부터 6개를 자른다.
+  return numbers;
+}
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-    </Head>
+const Home = () => {
+  const [list, setList] = React.useState(generate()); // state 선언
+  const regenerate = () => {
+    // state 에 값 넣기
+    setList(generate());
+  }
 
-    <div class="hero">
-      {numbers.join(', ')}
+  return (
+    // 리액트 : 변경이 되는 모든것은 state를 사용하길 권장. 사용안해도 되긴 하는데 ^^.@
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <div class="hero">
+        {list.join(', ')}
+      </div>
+
+      <div>
+        <button onClick={regenerate}>재생성</button>
+        {/* <button onClick={() => setList(generate())}></button> 로 작성해도 되지 않나요? 의문을 가질 수 있는데.. 취향의 차이다!*/}
+      </div>
+
+      <style jsx>{`
+    .hero {
+      width: 100%;
+      color: #333;
+    }
+    .title {
+      margin: 0;
+      width: 100%;
+      padding-top: 80px;
+      line-height: 1.15;
+      font-size: 48px;
+    }
+    .title,
+    .description {
+      text-align: center;
+    }
+    .row {
+      max-width: 880px;
+      margin: 80px auto 40px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+    }
+    .card {
+      padding: 18px 18px 24px;
+      width: 220px;
+      text-align: left;
+      text-decoration: none;
+      color: #434343;
+      border: 1px solid #9b9b9b;
+    }
+    .card:hover {
+      border-color: #067df7;
+    }
+    .card h3 {
+      margin: 0;
+      color: #067df7;
+      font-size: 18px;
+    }
+    .card p {
+      margin: 0;
+      padding: 12px 0 0;
+      font-size: 13px;
+      color: #333;
+    }
+  `}</style>
     </div>
-
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+  )
+}
 
 export default Home
